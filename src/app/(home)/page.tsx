b39c222b769b78b138d407d1eb7b6f6872bc2db0
IDNK2203 <efeidukpaye@gmail.com>
2024-02-16 09:10:56 +0100

    fixed sync btween dropdown the searchparams

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -16,45 +16,45 @@ export default function Page({
   const toggleCreateModal = useCreateModalDispatch();
 
   return (
-    <main className='flex-1 flex h-full flex-col items-center py-8 pb-20 md:p-24 sm:py-16 bg-[--primary_bg] overflow-y-scroll'>
-      <div className='w-full max-w-3xl px-4'>
-        <div className='my-8 flex justify-between'>
-          <div className='text-[--primary_fg]'>
-            <h2 className='text-xl md:text-4xl font-medium'>Invoices</h2>
+    <main className="flex-1 flex h-full flex-col items-center py-8 pb-20 md:p-24 sm:py-16 bg-[--primary_bg] overflow-y-scroll">
+      <div className="w-full max-w-3xl px-4">
+        <div className="my-8 flex justify-between">
+          <div className="text-[--primary_fg]">
+            <h2 className="text-xl md:text-4xl font-medium">Invoices</h2>
             <p>
               {" "}
-              <span className='md:hidden'>
+              <span className="md:hidden">
                 {filteredInvoices.length} Invoices
               </span>{" "}
-              <span className='hidden md:inline'>
+              <span className="hidden md:inline">
                 There are {filteredInvoices.length} total invoices.
               </span>
             </p>
           </div>
-          <div className='flex flex-wrap items-center'>
-            <StatusDropdown />
-            <div className='ml-2 md:ml-6'>
+          <div className="flex flex-wrap items-center">
+            <StatusDropdown searchParamsList={status} />
+            <div className="ml-2 md:ml-6">
               <Cbutton
                 funcHandler={toggleCreateModal}
-                type='primary'
-                classes='justify-between items-center px-2 md:pl-2 md:pr-4 md:w-[150px]'
+                type="primary"
+                classes="justify-between items-center px-2 md:pl-2 md:pr-4 md:w-[150px]"
               >
-                <span className='h-8 w-8 relative rounded-full bg-white'>
+                <span className="h-8 w-8 relative rounded-full bg-white">
                   <svg
-                    className='absolute left-1/2 top-1/2 -translate-x-[5px] -translate-y-[5px]  '
-                    width='12'
-                    height='12'
-                    xmlns='http://www.w3.org/2000/svg'
+                    className="absolute left-1/2 top-1/2 -translate-x-[5px] -translate-y-[5px]  "
+                    width="12"
+                    height="12"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      d='M6.313 10.023v-3.71h3.71v-2.58h-3.71V.023h-2.58v3.71H.023v2.58h3.71v3.71z'
-                      fill='#7C5DFA'
-                      fillRule='nonzero'
+                      d="M6.313 10.023v-3.71h3.71v-2.58h-3.71V.023h-2.58v3.71H.023v2.58h3.71v3.71z"
+                      fill="#7C5DFA"
+                      fillRule="nonzero"
                     />
                   </svg>
                 </span>
-                <span className='hidden sm:max-md:inline'> New</span>
-                <span className='hidden md:inline'>New Invoices</span>
+                <span className="hidden sm:max-md:inline"> New</span>
+                <span className="hidden md:inline">New Invoices</span>
               </Cbutton>
             </div>
           </div>
--- src/components/allInvoices/statusDropdown.tsx
+++ src/components/allInvoices/statusDropdown.tsx
@@ -3,74 +3,87 @@ import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
 import chevronDown from "../../../public/assets/icon-arrow-down.svg";
 
-type InputObject = {
-  [key: string]: any;
+type statusType = "paid" | "pending" | "draft" | "all";
+interface IStatus {
+  paid: boolean,
+  pending: boolean,
+  draft: boolean,
+  all: boolean,
 };
 
-function getTruthyValues(obj: InputObject): any[] {
-  return Object.keys(obj).filter((key) => Boolean(obj[key]));
-}
-export default function StatusDropdown() {
+const statusOptions: statusType[] = ["all", "paid", "pending", "draft"];
+
+export default function StatusDropdown({
+  searchParamsList,
+}: {
+  searchParamsList: string[];
+}) {
   const pathname = usePathname();
   const { replace } = useRouter();
   const searchParams = useSearchParams();
-  const [Istatus, setIstatus] = useState<Record<string, boolean>>({
-    paid: false,
-    pending: false,
-    draft: false,
-    all: true,
+
+
+  
+  const [status, setStatus] = useState<IStatus>(() => {
+    const defaultStatus = statusOptions.reduce((acc, status) => {
+      acc[status] = searchParamsList?.includes(status) ?? false;
+      return acc;
+    }, {
+      paid: false,
+      pending: false,
+      draft: false,
+      all: false,
+    });
+    return defaultStatus;
   });
 
-  function handleStatusChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setIstatus((status) => {
-      return {
-        ...status,
-        [e.target.name]: !status[e.target.name],
-      };
-    });
-  }
-
-  const dropdownData = ["all", "paid", "pending", "draft"];
-  // "checked:border-indigo-800 [--chkbg:theme(colors.indigo.600)] [--chkfg:orange]"
+  const handleStatusChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const s = e.target.name as any as statusType;
+    setStatus((prevStatus) => ({ ...prevStatus, [s]: !prevStatus[s] }));
+  };
 
   useEffect(() => {
     const params = new URLSearchParams(searchParams);
-    const filteredStatus = getTruthyValues(Istatus).join(",");
+    const filteredStatus = Object.entries(status)
+      .filter(([_, value]) => value)
+      .map(([key]) => key)
+      .join(",");
     params.set("status", filteredStatus);
     replace(`${pathname}?${params.toString()}`);
-  }, [Istatus, replace, pathname, searchParams]);
+  }, [status, replace, pathname, searchParams]);
+
   return (
     <div>
-      <div className='dropdown dropdown-hover group'>
+      <div className="dropdown dropdown-hover group">
         <div
           tabIndex={0}
-          role='button'
-          className='btn btn-ghost my-1 md:m-1 px-2 md:px-4 text-[--primary_fg]'
+          role="button"
+          className="btn btn-ghost my-1 md:m-1 px-2 md:px-4 text-[--primary_fg]"
         >
           Filter by status
           <Image
-            className=' transition origin-center group-hover:rotate-180'
+            className="transition origin-center group-hover:rotate-180"
             src={chevronDown}
-            alt='Arrow Indicator'
+            alt="Arrow Indicator"
           />
         </div>
         <ul
           tabIndex={0}
-          className='p-2 shadow menu bg-[--secondary_bg_700] dropdown-content z-[1] rounded-md w-52'
+          className="p-2 shadow menu bg-[--secondary_bg_700] dropdown-content z-[1] rounded-md w-52"
         >
-          {dropdownData.map((status) => (
-            <li key={status}>
-              <div className='form-control py-0 hover:bg-inherit active:bg-inherit focus:bg-inherit'>
-                <label className='label cursor-pointer'>
+          {statusOptions.map((s) => (
+            <li key={s}>
+              <div className="form-control py-0 hover:bg-inherit active:bg-inherit focus:bg-inherit">
+                <label className="label cursor-pointer">
                   <input
-                    type='checkbox'
-                    name={status}
-                    checked={Istatus[`${status}`]}
+                    type="checkbox"
+                    name={s}
+                    checked={status[s]}
                     onChange={handleStatusChange}
-                    className='checkbox checkbox-sm rounded-none hover:border-accent [--chkbg:theme(colors.accent)] [--chkfg:white]'
+                    className="checkbox checkbox-sm rounded-none hover:border-accent [--chkbg:theme(colors.accent)] [--chkfg:white]"
                   />
-                  <span className='label-text mx-4 font-bold text-[--primary_fg] capitalize'>
-                    {status}{" "}
+                  <span className="label-text mx-4 font-bold text-[--primary_fg] capitalize">
+                    {s}
                   </span>
                 </label>
               </div>
